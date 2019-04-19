1ab0ecf26887a6b55d230cf8d47f1d9f569e6f88
Rizki Ramadhan <rizkyramadhan028@gmail.com>
2019-04-19 16:43:15 +0700

    Blind-Hiker vFinal

--- Blind-Hiker.py
+++ Blind-Hiker.py
@@ -3,40 +3,42 @@ import pylab
 import random 
   
 #number of steps 
-n = 10000
+n = 100
   
 #creating two array for x and y coordinate, filled with 0
 x = numpy.zeros(n) 
 y = numpy.zeros(n) 
-  
+
+
 # filling the coordinates
 for i in range(1, n): 
-    val = random.randint(1, 8) 
-    if val == 1: 
+    val = random.uniform(0, 100) 
+    print("Random Value: ",val)
+    if val < 12.5 : 
         #north
         x[i] = x[i - 1]
         y[i] = y[i - 1] + 1
-    elif val == 2: 
+    elif val < 25 : 
         #north east
         x[i] = x[i - 1] + 1
         y[i] = y[i - 1] + 1
-    elif val == 3: 
+    elif val < 37.5: 
         #east
         x[i] = x[i - 1] + 1
         y[i] = y[i - 1] 
-    elif val == 4:
+    elif val < 50:
         #south east
         x[i] = x[i - 1] + 1
         y[i] = y[i - 1] - 1
-    elif val == 5: 
+    elif val < 62.5: 
         #south
         x[i] = x[i - 1] 
         y[i] = y[i - 1] - 1
-    elif val == 6:
+    elif val < 75:
         #south west 
         x[i] = x[i - 1] - 1
-        y[i] = y[i - 1] + 1
-    elif val == 7:
+        y[i] = y[i - 1] - 1
+    elif val < 87.5:
         #west
         x[i] = x[i - 1] - 1
         y[i] = y[i - 1] 
@@ -45,9 +47,14 @@ for i in range(1, n):
         x[i] = x[i - 1] - 1
         y[i] = y[i - 1] + 1
       
-  
-# plotting stuff: 
+# output:
+print("Start")
+for a in range (0,n):
+    print("Coordinate:","(",x[a]," , ",y[a],")")
+print("End")
+
+# plotting: 
 pylab.title("Random Walk ($n = " + str(n) + "$ steps)") 
 pylab.plot(x, y) 
-pylab.savefig("random_walk"+str(n)+".png",bbox_inches="tight",dpi=600) 
+pylab.savefig("Rizki_Ramadhan__random_walk.png",bbox_inches="tight",dpi=300) 
 pylab.show() 
